6f4ecd1f9d23b92c13a2c97521931b3e3e664206
Drewisatlas <drewisatlas@gmail.com>
2018-10-25 10:00:04 -0400

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
-# Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  @@all = []
+
+  def initialize(limit)
+    @limit = limit
+    @multiples = nil
+    @@all << self
+  end
+
+  def collect_multiples
+    range = (1...limit)
+    range.select {|number| number % 3 == 0 || number % 5 == 0}
+  end
+
+  def sum_multiples
+    self.collect_multiples.reduce(:+)
+  end
+end
