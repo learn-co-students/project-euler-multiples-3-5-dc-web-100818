af6e5c4a9db946e0d35efb4538649eadab85a854
Joseph Arias <arias.joseph.a@gmail.com>
2019-07-29 18:49:25 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
 # Enter your procedural solution here!
+
+
+def collect_multiples(number) 
+  list = (1...number).to_a
+  newList = list.select do |num| 
+    if num % 3 === 0 || num % 5 === 0
+      num
+    end
+  end 
+
+  return newList
+end 
+
+def sum_multiples(number)
+  multiples = collect_multiples(number)
+  return multiples.sum
+end
+
+
+
+
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
 # Enter your object-oriented solution here!
+
+class Multiples 
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def collect_multiples() 
+    list = (1...self.number).to_a
+    newList = list.select do |num| 
+      if num % 3 === 0 || num % 5 === 0
+        num
+      end
+    end 
+  
+    return newList
+  end 
+
+  def sum_multiples
+    multiples = self.collect_multiples()
+    return multiples.sum
+  end
+end
